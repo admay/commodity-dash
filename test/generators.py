625c6bca81ee6786c778bdf1e1f98c82b488c606
Michael Zavarella <michael.zavarella@capitalone.com>
2019-04-10 20:42:30 -0400

    Add sanatize tests

--- test/generators.py
+++ test/generators.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-def sanatize_dataframe(df):
+def gen_df():
+    df = pd.read_csv("./test-data.csv")
     df.columns = df.columns.map(lambda col: col.upper())
     df['DATE'] = pd.to_datetime(df['DATE'])
     df['YEAR'], df['MONTH'], df['DAY'] = df['DATE'].dt.year, df['DATE'].dt.month, df['DATE'].dt.day
     return df
 
-def get_headers(df):
-    return list(df.drop(columns=['DATE', 'YEAR', 'MONTH', 'DAY']))
