6c63d553749c84d7f4558f00fac57c20aedb4200
Michael Zavarella <michael.zavarella@capitalone.com>
2019-04-10 20:07:10 -0400

    Remove function not being used

--- sanatize.py
+++ sanatize.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-def validate_csv(csv):
-    return true
-
 def sanatize_dataframe(df):
     df.columns = df.columns.map(lambda col: col.upper())
     df['DATE'] = pd.to_datetime(df['DATE'])
