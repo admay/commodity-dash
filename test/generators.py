196a70105d3bd75ef9f50d864db5593137683572
Michael Zavarella <michael.zavarella@capitalone.com>
2019-04-16 11:58:49 -0400

    Update generator to meet linting

--- test/generators.py
+++ test/generators.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
+
 def gen_df():
     df = pd.read_csv("./test-data.csv")
     df.columns = df.columns.map(lambda col: col.upper())
     df['DATE'] = pd.to_datetime(df['DATE'])
-    df['YEAR'], df['MONTH'], df['DAY'] = df['DATE'].dt.year, df['DATE'].dt.month, df['DATE'].dt.day
+    df['YEAR'] = df['DATE'].dt.year
+    df['MONTH'] = df['DATE'].dt.month
+    df['DAY'] = df['DATE'].dt.day
     return df
-
